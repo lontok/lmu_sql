bbcb631e9db131433669996569d35bb84815ef2b
lontok <lontok@users.noreply.github.com>
2020-04-19 23:18:51 -0700

    Add files via upload

---
+++ lecture_exercises_transactions_triggers_fname_lname.sql
@@ -0,0 +1,29 @@
+################
+# TRANSACTIONS
+################
+
+# Example 1
+# Insert a new category named "Food" with a transaction
+
+
+
+# Example 2
+# Create a transaction to attempt an UPDATE on the Food category. 
+# Set the CategoryDescription to "Drinks"
+
+
+
+# Example 3
+# Update the example where you inserted a new bike product with 
+# variables and prepared statements to use a basic transaction
+
+
+
+################
+# TRIGGERS
+################
+
+# Example 4
+# Create a trigger to keep track of QuantityOrdered updates in the Order_Details table
+
+
