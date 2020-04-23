a945d24d4946782bb75c2d68fdf42aa23725272b
lontok <lontok@users.noreply.github.com>
2020-04-22 23:15:35 -0700

    Add files via upload

---
+++ lecture_exercises_stored_procedures_fname_lname.sql
@@ -0,0 +1,42 @@
+#####################
+# STORED PROCEDURES #
+#####################
+
+##################################
+# Example 1
+# Create a stored procedure to return the number of rows in the orders table
+
+
+
+# Execute the stored procedure
+
+
+
+##################################
+# Example 2
+# Update the ordersCount stored procedure to accept IN parameters for an orderDate date range
+
+
+
+# Execute the stored procedure
+
+
+
+# Verify results with a SELECT
+
+
+
+##################################
+# Example 3
+# Update the ordersCount stored procedure to return an OUT parameter for the number of rows in the orders table.
+# Keep the IN parameters for an orderDate date range.
+
+
+
+
+# Execute the stored procedure
+
+
+
+# Confirm the order count was assigned to the OUT parameter
+
